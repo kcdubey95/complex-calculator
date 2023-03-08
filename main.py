5b382175a8e22bc6976208cf9ef3e7207d7d584b
kcdubey95 <dubey.krishnachand@gmail.com>
2023-03-08 07:16:34 +0530

    calculater equal to function start

---
+++ main.py
@@ -0,0 +1,47 @@
+from tkinter import *
+import math
+
+tk = Tk()
+tk.title("Complex Calculater")
+e = Entry(tk, width=150, borderwidth=10)
+e.grid(row=0, columnspan=2, rowspan=5, padx=5, pady=5)
+
+tk.mainloop()
+
+
+# function start
+
+def thebutton(number):
+    the_current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(the_current) + str(number))
+
+
+def clear_button():
+    e.delete(0, END)
+
+
+def the_button_adds():
+    num1 = e.get()
+    global functionnum
+    global mathematics
+    mathematics = "addition"
+    functionnum = int(num1)
+    e.delete(0, END)
+
+
+def equal_to():
+    num2 = e.get()
+    e.delete(0, END)
+    if mathematics == "addition":
+        e.insert(0, functionnum+int(num2))
+    if mathematics == "subtraction":
+        e.insert(0, functionnum - int(num2))
+    if mathematics == "multiplication":
+        e.insert(0, functionnum * int(num2))
+    if mathematics == "division":
+        e.insert(0, functionnum / int(num2))
+
+
+
+
